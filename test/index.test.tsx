0c8e0aaa38289b0a56e34fd73f7bfde38723e2f0
Jason R. Stevens, CFA <jason@thinkjrs.dev>
2023-11-19 13:51:59 -0500

    :white_check_mark: add type function instantiation unit tests

--- test/index.test.tsx
+++ test/index.test.tsx
@@ -1,8 +1,425 @@
 import React from 'react';
+import {
+  CloudinaryOptions,
+  HowItWorksContent,
+  InputValues,
+  DashboardOptions,
+  CampaignSortedData,
+  Settings,
+  CampaignDummyData,
+  CampaignData,
+  CampaignMetadata,
+  CampaignStatsData,
+  CampaignMetrics,
+  InputConfig,
+  PromoOptions,
+  ButtonFormElements,
+  ButtonFormSubmission,
+  SocialKey,
+  SocialsType,
+  HeroiconIcon,
+  DownloadableCampaignStatsSample,
+  DownloadableCampaignMetadataSample,
+  PromoApiCampaignStatsSample,
+  CampaignStatsSample,
+  PromoApiCampaignStatsData,
+  DownloadableCampaignStats,
+} from '../dist/index';
 
-describe('Types', () => {
-  it('renders without crashing and single campaign data', () => {
-    const type = {}
-    expect(type).toBeDefined();
+// Mock function that accepts CloudinaryOptions
+function setupCloudinary(options: CloudinaryOptions) {
+  // implementation
+}
+
+// Test for CloudinaryOptions
+describe('setupCloudinary', () => {
+  it('should accept valid CloudinaryOptions', () => {
+    const options: CloudinaryOptions = {
+      cloudName: 'testCloud',
+      uploadPreset: 'preset1',
+      folder: 'folder1',
+      multiple: false,
+    };
+
+    expect(() => setupCloudinary(options)).not.toThrow();
+  });
+});
+
+// Mock function that accepts HowItWorksContent
+function displayHowItWorks(content: HowItWorksContent) {
+  // implementation
+}
+
+// Test for HowItWorksContent
+describe('displayHowItWorks', () => {
+  it('should accept valid HowItWorksContent', () => {
+    const content: HowItWorksContent = {
+      steps: [
+        { title: 'Step 1', subtitle: 'Description 1' },
+        { title: 'Step 2', subtitle: 'Description 2' },
+        { title: 'Step 3', subtitle: 'Description 3' },
+      ],
+      title: 'Main Title',
+      subtitle: 'Main Subtitle',
+      submittedSubtitle: 'Submitted Subtitle',
+      submittedTitle: 'Submitted Title',
+      // Optional footerCloseMessage can be added for more tests
+    };
+
+    expect(() => displayHowItWorks(content)).not.toThrow();
+  });
+});
+
+// Mock function that accepts InputValues
+function processInputValues(values: InputValues) {
+  // implementation
+}
+
+// Test for InputValues
+describe('processInputValues', () => {
+  it('should accept valid InputValues', () => {
+    const values: InputValues = {
+      adTitle: 'Ad Title',
+      budget: 1000,
+      target: 'http://example.com',
+      adCopy: 'Ad Copy',
+      adCallToAction: 'Call to Action',
+      buttonText: 'Click Here',
+      creativeUri: [],
+      creativeUrls: ['http://image1.com', 'http://image2.com'],
+      isFlat: true,
+    };
+
+    expect(() => processInputValues(values)).not.toThrow();
+  });
+});
+
+function setupDashboard(options: DashboardOptions) {
+  // Mock implementation
+}
+
+describe('setupDashboard', () => {
+  it('should accept valid DashboardOptions', () => {
+    const options: DashboardOptions = {
+      emailDomain: 'example.com',
+      // ... other properties
+    };
+
+    expect(() => setupDashboard(options)).not.toThrow();
+  });
+});
+
+function processCampaignSortedData(data: CampaignSortedData) {
+  // Mock implementation
+}
+
+describe('processCampaignSortedData', () => {
+  it('should accept valid CampaignSortedData', () => {
+    const data: CampaignSortedData = {
+      // ... properties according to CampaignSortedData type
+    };
+
+    expect(() => processCampaignSortedData(data)).not.toThrow();
+  });
+});
+
+function configureSettings(settings: Settings) {
+  // Mock implementation
+}
+
+describe('configureSettings', () => {
+  it('should accept valid Settings', () => {
+    const settings: Settings = {
+      fullName: 'John Doe',
+      // ... other properties
+    };
+
+    expect(() => configureSettings(settings)).not.toThrow();
+  });
+});
+
+function handleCampaignDummyData(data: CampaignDummyData) {
+  // Mock implementation
+}
+
+describe('handleCampaignDummyData', () => {
+  it('should accept valid CampaignDummyData', () => {
+    const data: CampaignDummyData = {
+      // ... properties according to CampaignDummyData type
+    };
+
+    expect(() => handleCampaignDummyData(data)).not.toThrow();
+  });
+});
+function processCampaignData(data: CampaignData) {
+  // Mock implementation
+}
+
+describe('processCampaignData', () => {
+  it('should accept valid CampaignData', () => {
+    const data: CampaignData = {
+      // ... properties according to CampaignData type
+    };
+
+    expect(() => processCampaignData(data)).not.toThrow();
+  });
+});
+function useCampaignMetadata(metadata: CampaignMetadata) {
+  // Mock implementation
+}
+
+describe('useCampaignMetadata', () => {
+  it('should accept valid CampaignMetadata', () => {
+    const metadata: CampaignMetadata = {
+      pid: '123',
+      // ... other properties
+    };
+
+    expect(() => useCampaignMetadata(metadata)).not.toThrow();
+  });
+});
+
+function displayCampaignStatsData(stats: CampaignStatsData) {
+  // Mock implementation
+}
+const MockIcon = React.forwardRef((props, ref) => (
+  // @ts-ignore
+  <svg ref={ref} {...props}></svg>
+));
+
+describe('displayCampaignStatsData', () => {
+  it('should accept valid CampaignStatsData', () => {
+    const stats: CampaignStatsData = {
+      id: 1,
+      name: 'Clicks', // Assuming 'Clicks' is one of the CampaignMetrics
+      stat: '500',
+      isActive: true, // @ts-ignore
+      icon: MockIcon,
+      change: 50,
+      changeType: 'increase',
+      chartData: {
+        labels: ['2023-03-01', '2023-03-02', '2023-03-03'],
+        data: [450, 475, 500],
+      },
+    };
+    expect(() => displayCampaignStatsData(stats)).not.toThrow();
+  });
+});
+
+function checkCampaignMetric(metric: CampaignMetrics) {
+  // Mock implementation
+}
+
+describe('checkCampaignMetric', () => {
+  it('should accept valid CampaignMetrics', () => {
+    const metric: CampaignMetrics = 'Clicks'; // Example metric
+
+    expect(() => checkCampaignMetric(metric)).not.toThrow();
+  });
+});
+
+function setupInputConfig(config: InputConfig) {
+  // Mock implementation
+}
+
+describe('setupInputConfig', () => {
+  it('should accept valid InputConfig', () => {
+    const config: InputConfig = {
+      minSpend: 100,
+      maxSpend: 1000,
+      rangeStep: 50,
+    };
+
+    expect(() => setupInputConfig(config)).not.toThrow();
+  });
+});
+
+function initializePromoOptions(options: PromoOptions) {
+  // Mock implementation
+}
+
+describe('initializePromoOptions', () => {
+  it('should accept valid PromoOptions', () => {
+    const options: PromoOptions = {
+      // ... properties according to PromoOptions type
+    };
+
+    expect(() => initializePromoOptions(options)).not.toThrow();
+  });
+});
+
+function processButtonFormElements(elements: ButtonFormElements) {
+  // Mock implementation
+}
+
+describe('processButtonFormElements', () => {
+  it('should accept valid ButtonFormElements', () => {
+    // Note: In a real test, you'd create a mock HTMLFormControlsCollection
+    const elements: any = {}; // Placeholder for a mock HTMLFormControlsCollection
+
+    expect(() =>
+      processButtonFormElements(elements as ButtonFormElements)
+    ).not.toThrow();
+  });
+});
+
+function handleSubmit(form: ButtonFormSubmission) {
+  // Mock implementation
+}
+
+describe('handleSubmit', () => {
+  it('should accept valid ButtonFormSubmission', () => {
+    // Note: In a real test, you'd create a mock HTMLFormElement
+    const form: any = {}; // Placeholder for a mock HTMLFormElement
+
+    expect(() => handleSubmit(form as ButtonFormSubmission)).not.toThrow();
+  });
+});
+
+function checkSocialKey(key: SocialKey) {
+  // Mock implementation
+}
+
+describe('checkSocialKey', () => {
+  it('should accept valid SocialKey', () => {
+    const key: SocialKey = 'Facebook';
+
+    expect(() => checkSocialKey(key)).not.toThrow();
+  });
+});
+
+function configureSocialsType(socials: SocialsType) {
+  // Mock implementation
+}
+
+describe('configureSocialsType', () => {
+  it('should accept valid SocialsType', () => {
+    const socials: SocialsType = {
+      Facebook: 'Facebook',
+      Twitter: 'Twitter',
+      // ... other properties
+    };
+
+    expect(() => configureSocialsType(socials)).not.toThrow();
+  });
+});
+
+//function renderHeroiconIcon(icon: HeroiconIcon) {
+//  // Mock implementation
+//}
+//
+//describe('renderHeroiconIcon', () => {
+//  it('should accept valid HeroiconIcon', () => {
+//    const icon: HeroiconIcon = {}; // Placeholder for a mock HeroiconIcon
+//
+//    expect(() => renderHeroiconIcon(icon)).not.toThrow();
+//  });
+//});
+
+function processDownloadableCampaignStatsSample(
+  stats: DownloadableCampaignStatsSample
+) {
+  // Mock implementation
+}
+
+describe('processDownloadableCampaignStatsSample', () => {
+  it('should accept valid DownloadableCampaignStatsSample', () => {
+    const stats: DownloadableCampaignStatsSample = {
+      pid: 'abcdefgh',
+      updatedTime: '2023-07-20T12:00:00Z',
+      updated_time: '2023-07-20T12:00:00Z', // Note: updatedTime and updated_time are redundant; choose one based on actual usage
+      spend: 1500,
+      reach: 10000,
+      views: 5000,
+      clicks: 200,
+      cpc: '0.75',
+      ctr: '4%',
+      cpm: '15',
+      cpv: '0.30',
+    };
+
+    expect(() => processDownloadableCampaignStatsSample(stats)).not.toThrow();
+  });
+});
+
+//function handleDownloadableCampaignMetadataSample(metadata: DownloadableCampaignMetadataSample) {
+//  // Mock implementation
+//}
+
+//describe('handleDownloadableCampaignMetadataSample', () => {
+//  it('should accept valid DownloadableCampaignMetadataSample', () => {
+//    const metadata: DownloadableCampaignMetadataSample = {
+//      pid: '123',
+//      // ... other properties
+//    };
+//
+//    expect(() => handleDownloadableCampaignMetadataSample(metadata)).not.toThrow();
+//  });
+//});
+
+function analyzePromoApiCampaignStatsSample(
+  stats: PromoApiCampaignStatsSample
+) {
+  // Mock implementation
+}
+
+describe('analyzePromoApiCampaignStatsSample', () => {
+  it('should accept valid PromoApiCampaignStatsSample', () => {
+    const stats: PromoApiCampaignStatsSample = {
+      // ... properties according to PromoApiCampaignStatsSample type
+    };
+
+    expect(() => analyzePromoApiCampaignStatsSample(stats)).not.toThrow();
+  });
+});
+
+function displayCampaignStatsSample(stats: CampaignStatsSample) {
+  // Mock implementation
+}
+
+describe('displayCampaignStatsSample', () => {
+  it('should accept valid CampaignStatsSample', () => {
+    const stats: CampaignStatsSample = {
+      name: 'Spend',
+      // ... other properties
+    };
+
+    expect(() => displayCampaignStatsSample(stats)).not.toThrow();
+  });
+});
+
+function processPromoApiCampaignStatsData(data: PromoApiCampaignStatsData) {
+  // Mock implementation
+}
+
+describe('processPromoApiCampaignStatsData', () => {
+  it('should accept valid PromoApiCampaignStatsData', () => {
+    const data: PromoApiCampaignStatsData = {
+      // ... properties according to PromoApiCampaignStatsData type
+    };
+
+    expect(() => processPromoApiCampaignStatsData(data)).not.toThrow();
+  });
+});
+
+function analyzeDownloadableCampaignStats(stats: DownloadableCampaignStats) {
+  // Mock implementation
+}
+
+describe('analyzeDownloadableCampaignStats', () => {
+  it('should accept valid DownloadableCampaignStats', () => {
+    const stats: DownloadableCampaignStats = {
+      updatedTime: ['2023-07-20T12:00:00Z', null, '2023-07-22T12:00:00Z'],
+      spend: [1500, '2000', null],
+      reach: [10000, null, '12000'],
+      views: ['5000', 5500, null],
+      clicks: [200, '250', null],
+      cpc: ['0.75', null, 0.8],
+      cpm: [null, '15', 20],
+      ctr: ['4%', null, '5%'],
+      cpv: ['0.30', 0.35, null],
+      pid: ['campaign123', null, 'campaign456'],
+    };
+    expect(() => analyzeDownloadableCampaignStats(stats)).not.toThrow();
   });
 });
